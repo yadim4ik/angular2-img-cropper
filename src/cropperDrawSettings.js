75c98ca13b47d5b5fd2cd71081bc1c7d6e3f2965
ya_dim4ik <ya.dim4ik@gmail.com>
2017-08-03 11:01:08 +0300

    update .gitignore

---
+++ src/cropperDrawSettings.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CropperDrawSettings = (function () {
+    function CropperDrawSettings() {
+        this.strokeWidth = 1;
+        this.strokeColor = 'rgba(255,255,255,1)';
+        this.dragIconStrokeWidth = 1;
+        this.dragIconStrokeColor = 'rgba(0,0,0,1)';
+        this.dragIconFillColor = 'rgba(255,255,255,1)';
+    }
+    return CropperDrawSettings;
+}());
+exports.CropperDrawSettings = CropperDrawSettings;
+//# sourceMappingURL=cropperDrawSettings.js.map
